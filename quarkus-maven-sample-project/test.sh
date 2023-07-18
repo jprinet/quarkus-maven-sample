6c8a19c20f2431d930613adcf9e6bbef4f8e17ec
Jerome Prinet <jprinet@gradle.com>
2023-07-18 18:25:17 +0200

    Add test script

---
+++ quarkus-maven-sample-project/test.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+function assertNoConfigDump() {
+    local buildLog=$1
+    local quarkusPropertiesNotFound=$(grep "Quarkus properties not found" ${buildLog} | wc -l)
+    local cachingNotPossible=$(grep "Caching not possible for Quarkus goal" ${buildLog} | wc -l)
+
+    if [ "${quarkusPropertiesNotFound}" == "0" ] || [ "${cachingNotPossible}" == "0" ]
+    then
+        echo "ERROR - config dump is present"
+        exit 1
+    fi
+}
+
+function assertCacheHitCount() {
+    local buildLog=$1
+    local count=$2
+    local cacheHit=$(grep "Configuring caching for Quarkus build" -A 1 ${buildLog} | grep "Loaded from the build cache" | wc -l)
+
+    if [ "${cacheHit}" != "${count}" ]
+    then
+        echo "ERROR - cache hit = ${cacheHit}"
+        exit 1
+    fi
+}
+
+function assertCacheMiss() {
+    assertCacheHitCount $1 0
+}
+
+function assertCacheHit() {
+    assertCacheHitCount $1 1
+}
+
+function assertPropertyChanged() {
+    local buildLog=$1
+    local quarkusProp=$2
+    local quarkusPropChanged=$(grep -E "Quarkus properties have changed.*${quarkusProp}" ${buildLog} | wc -l)
+
+    if [ "$quarkusPropChanged" != "1" ]
+    then
+        echo "ERROR - property change not detected"
+        exit 1
+    fi
+}
+
+# Clean local cache
+rm -rf ~/.m2/.gradle-enterprise/build-cache
+
+# Clean quarkus dump
+rm -rf .quarkus/*-config-dump
+
+# Clean log files
+rm -f /tmp/build*.log
+
+# Run build 1
+echo "Run build 1 - Expect no config dump"
+buildLog="/tmp/build1.log"
+./mvnw -B clean package -DskipTests 2>&1 | tee -a ${buildLog}
+assertNoConfigDump ${buildLog}
+
+#FIXME Remove run build 2 when dump file will be like check file (~ vs path)
+#echo "Run build 1 bis - Expect cache miss"
+#buildLog="/tmp/build1bis.log"
+#./mvnw -B clean package -DskipTests 2>&1 | tee -a ${buildLog}
+#assertCacheMiss ${buildLog}
+
+# Run build 2
+echo "Run build 2 - Expect cache miss"
+buildLog="/tmp/build2.log"
+./mvnw -B clean package -DskipTests 2>&1 | tee -a ${buildLog}
+assertCacheMiss ${buildLog}
+
+# Run build 3
+echo "Run build 3 - Expect cache hit"
+buildLog="/tmp/build3.log"
+./mvnw -B clean package -DskipTests 2>&1 | tee -a ${buildLog}
+assertCacheHit ${buildLog}
+#FIXME previous cache entry not stored due to Goal execution marked as not cacheable: Build caching was not enabled for this goal execution because pre-existing files were modified. Cacheable goals may only create new files.
+#assertCacheMiss ${buildLog}
+
+# Run build 3 bis
+#echo "Run build 3 bis - Expect cache hit"
+#buildLog="/tmp/build3bis.log"
+#./mvnw -B clean package -DskipTests 2>&1 | tee -a ${buildLog}
+#assertCacheHit ${buildLog}
+
+# Run build 4
+echo "Run build 4 - Expect property changed"
+buildLog="/tmp/build4.log"
+quarkusProperty="quarkus.live-reload.retry-interval"
+./mvnw -B clean package -DskipTests -D${quarkusProperty}=15s 2>&1 | tee -a ${buildLog}
+assertPropertyChanged ${buildLog} ${quarkusProperty}
+
+# Run build 5
+echo "Run build 5 - Expect cache hit"
+buildLog="/tmp/build5.log"
+./mvnw -B clean package -DskipTests -D${quarkusProperty}=15s 2>&1 | tee -a ${buildLog}
+assertCacheHit ${buildLog}
+
+# Run build 6
+echo "Run build 6 - Expect cache miss"
+buildLog="/tmp/build6.log"
+./mvnw -B clean package -DskipTests -Dnative -D${quarkusProperty}=15s 2>&1 | tee -a ${buildLog}
+assertCacheMiss ${buildLog}
+# => FAIL due to property changed not detected
+# dump file contains quarkus.package.type=uber-jar instead of native
+
+# Run build 7
+echo "Run build 7 - Expect cache hit"
+buildLog="/tmp/build7.log"
+./mvnw -B clean package -DskipTests -Dnative -D${quarkusProperty}=15s 2>&1 | tee -a ${buildLog}
+assertCacheHit ${buildLog}
+
+# Run build 8
+rm -rf .quarkus/*-config-dump
+echo "Run build 8 - Expect no config dump"
+buildLog="/tmp/build8.log"
+./mvnw -B clean package -DskipTests -Dnative 2>&1 | tee -a ${buildLog}
+assertNoConfigDump ${buildLog}
+
+echo "TEST SUCCESSFUL"
